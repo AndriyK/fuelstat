bc546a4d5897502fec67be3e7265b68f26904f1b
Andriy Kondratovets <kondratovets.andrey@gmail.com>
2019-09-03 21:53:39 +0300

    Updates

---
+++ precache-manifest.e2f95cac1bf230e41fd32f2e45843c34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6297e28abb609d75eda4de27e857fb",
+    "url": "/fuelstat/index.html"
+  },
+  {
+    "revision": "adb975726527fc3d390e",
+    "url": "/fuelstat/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5c722f06060fa388096a",
+    "url": "/fuelstat/static/js/2.2ab0f3a4.chunk.js"
+  },
+  {
+    "revision": "adb975726527fc3d390e",
+    "url": "/fuelstat/static/js/main.6fc2f4ab.chunk.js"
+  },
+  {
+    "revision": "e10324191d46cc21a063",
+    "url": "/fuelstat/static/js/runtime~main.c82ca244.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fuelstat/static/media/logo.5d5d9eef.svg"
+  }
+]);
