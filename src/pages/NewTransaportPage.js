c66aa50ec4783f3ceb343fdf699d9b837fda694c
Andriy Kondratovets <kondratovets.andrey@gmail.com>
2019-09-06 22:54:29 +0300

    add form for adding new vehicle

--- src/pages/NewTransaportPage.js
+++ src/pages/NewTransaportPage.js
@@ -1,13 +1,80 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+}));
 
 function NewTransportPage() {
+  const classes = useStyles();
+
   return (
     <div>
-    	It is new transaport page
-    	<br />
-    	<Link to="/">Return to Home</Link>
+    	<AppBar position="static">
+    		<Toolbar>
+    			<Typography variant="h6" >New vehicle</Typography>
+    		</Toolbar>
+    	</AppBar>
+	    <form >
+		    <TextField
+		        id="transport-name"
+		        label="Name"
+		        defaultValue=""
+		        margin="normal"
+		        className={classes.textField}
+		    />
+		    <br />
+		    <TextField
+		        id="transport-engine"
+		        label="Engine"
+		        defaultValue=""
+		        margin="normal"
+		        className={classes.textField}
+		    />
+	    	<br />
+	    	<TextField
+		        id="mileage"
+		        label="Mileage"
+		        defaultValue=""
+		        margin="normal"
+		        className={classes.textField}
+		    />
+	    	<br />
+	    	<Button
+			  variant="outlined"
+			  component="label"
+			  size="small"
+			  style={{ margin: "10px" }}
+			>
+			  Upload image
+			  <input
+			    type="file"
+			    style={{ display: "none" }}
+			  />
+			</Button>
+			<br />
+			<Button variant="contained" color="primary" style={{ margin: "10px", marginLeft: "70px" }}>
+	          Add
+	        </Button>
+	        <br />
+	    	<Link to="/">Return to Home</Link>
+	    </form>    	
     </div>
+
   );
 }
 
